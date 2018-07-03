48857a49b5c25b925c68fd4fd39e70fa9481fd32
Aviv Moyal <aviv.moyak@gmail.com>
2018-07-03 19:59:32 +0300

    ed

--- app/src/main/java/findyourhall/findyourhall/MainActivity.java
+++ app/src/main/java/findyourhall/findyourhall/MainActivity.java
@@ -1,8 +1,7 @@
 package findyourhall.findyourhall;
 
-import android.content.SyncStatusObserver;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
